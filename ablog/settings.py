6af13774ddbe695e82a860bc3a1ae72fc42f7de7
mohnish pund <mohnishpund11@gmail.com>
2022-06-19 04:21:30 -0700

    tweaked heroku

--- ablog/settings.py
+++ ablog/settings.py
@@ -12,6 +12,9 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 
 from pathlib import Path
 import os
+import django_heroku
+import dj_database_url
+from decouple import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -52,6 +55,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'whitenoise.middleware.WhiteNoiceMiddleware',
 ]
 
 ROOT_URLCONF = 'ablog.urls'
@@ -135,6 +139,7 @@ STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'static'),
     )
 
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorege'
 
 # Email settings
 EMAIL_HOST = 'smtp.gmail.com'
@@ -143,6 +148,7 @@ EMAIL_HOST_USER = 'mohnishpund2020@gmail.com'
 EMAIL_HOST_PASSWORD = 'Pund@123'
 EMAIL_USE_TLS = True
 #EMAIL_USE_SSL = False
+
 
 
 LOGIN_REDIRECT_URL = 'home'
@@ -154,3 +160,6 @@ LOGOUT_REDIRECT_URL = 'home'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+django_heroku.settings(locals())
